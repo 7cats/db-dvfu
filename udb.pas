096eed45a25297220b77bdd276aac63703af82cc
7cats <acmxca@gmail.com>
2016-03-27 15:55:53 +1000

    Added unit for db connection

---
+++ udb.pas
@@ -0,0 +1,42 @@
+unit udb;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+    Classes, SysUtils, IBConnection, sqldb, db;
+
+type
+    TDataBase = class
+        IBConnection: TIBConnection;
+        SQLTransaction: TSQLTransaction;
+        procedure Connect;
+    end;
+
+var
+    DataBase:TDataBase;
+
+implementation
+
+procedure TDataBase.Connect;
+begin
+    try
+        IBConnection.Password := 'masterkey';
+        IBConnection.CharSet:='UTF8';
+        IBConnection.UserName := 'SYSDBA';
+        IBConnection.DatabaseName := '../MY.FDB';
+        IBConnection.Transaction := DataBase.SQLTransaction;
+        SQLTransaction.DataBase := IBConnection;
+    except
+        Raise Exception.Create('Problem with connect to database');
+    end;
+end;
+
+initialization
+    DataBase := TDataBase.Create();
+    DataBase.IBConnection := TIBConnection.Create(nil);
+    DataBase.SQLTransaction := TSQLTransaction.Create(nil);
+end.
+
+
