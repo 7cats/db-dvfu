0224463946bb8a9e29d3bb258e23f90df594e19e
7cats <acmxca@gmail.com>
2016-05-02 17:26:54 +1000

    Added construstor and controls in the TEditForm

--- ueditform.pas
+++ ueditform.pas
@@ -14,14 +14,36 @@ type
     {TEditForm}
 
     TEditForm = class(TForm)
+        TableNameLabel : TLabel;
+        SaveBtn : TButton;
+        CancelBtn : TButton;
+        constructor CreateNew(Component : TComponent; tableName : TCaption);
         public
 
         private
+            procedure CreateFieldChangers(tableName : string);
     end;
 var
     EditForm : TEditForm;
 implementation
 
 {$R *.lfm}
+
+{ TEditForm }
+
+constructor TEditForm.CreateNew(Component : TComponent; tableName : TCaption);
+begin
+    Inherited Create(Component);
+    Self.Caption := (Component as TButton).Caption + ' поле в таблицу';
+    TableNameLabel.Caption := TableNameLabel.Caption + tableName;
+
+    //CreteFieldChangers(tableName);
+    Self.Show();
+end;
+
+procedure TEditForm.CreateFieldChangers(tableName : string);
+begin
+
+end;
 
 end.
